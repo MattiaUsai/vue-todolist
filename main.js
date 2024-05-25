0fb3f45cc958d31d5ec330fa9302ffee3f6b4c82
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-05-25 13:09:37 +0200

    added filter and new task

--- main.js
+++ main.js
@@ -24,22 +24,72 @@ createApp({
                 
 
             ],
-            ciao:true
+            ciao:true,
+            findTask:'',
+            newItem:'',
+            addTaskQuestion:false,
             
         }
 
     },
     methods:{
         changeCheck(index){
-            console.log(this.todoItems[index].check)
             this.todoItems[index].check = !this.todoItems[index].check
-            console.log(this.todoItems[index].check)
         },
 
         deleteItems(i){
            this.todoItems = this.todoItems.filter((_,index)   =>  index !== i)
 
-        }
+        },
+
+        addTask(){
+            console.log('non esiste la task vuoi agginugerla')
+
+            const newItem ={
+                text:this.findTask,
+                check:false
+            }
+
+
+
+
+
+
+
+            this.todoItems.push(newItem)
+
+
+        },
+        
+
+
+        filterTask(){
+            if(this.findTask){
+
+                const taskFiltered= this.todoItems.filter((item)=>{
+                    return item.text.toLowerCase().includes(this.findTask.toLowerCase());
+                })
+                console.log(taskFiltered.length)
+
+                if (taskFiltered.length === 0){
+                    this.addTaskQuestion =  true;
+                }else{
+                    this.addTaskQuestion =  false;
+
+                    return taskFiltered
+
+                }
+
+            } else{
+                this.addTaskQuestion =  false;
+                
+
+                return this.todoItems;
+            }
+
+
+        },
+        
 
 
     }
